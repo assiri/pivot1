dd90ce68cf48fbfa7f97d70cdbcf31f4c228e753
assiri <assiri@hotmail.com>
2022-08-02 03:24:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { PivotViewComponent, FieldList, CalculatedField, Inject } from '@syncfusion/ej2-react-pivotview';
+import {pivotData} from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ return (
+    <div id="wrapper">
+      <PivotViewComponent 
+        height={'300'}
+        width={'100%'}
+        dataSourceSettings={{
+          dataSource: pivotData,
+          rows: [{name:"Country"}, {name: "Products"}],
+          columns:[{name: "Year"}],
+          values: [{name: "Sold", caption: "Units Sold"}, {name: "Amount", caption: "Sold Amount"}, {name: "Total", caption: "Total Units", type: "CalculatedField"}],
+          filters: [{name: "Quarter"}],
+          calculatedFieldSettings: [{
+            name: "Total",
+            formula: '"Sum(Amount)"+"Sum(Sold)"' 
+          }]
+        }}
+        showFieldList={true}
+        allowCalculatedField={true}>
+          <Inject services={[FieldList, CalculatedField]}></Inject>
+      </PivotViewComponent>
     </div>
   );
 }
